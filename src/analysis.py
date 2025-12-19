be90e9f19b7ca0a96fa97aa453d7c5f5a8f19306
Ishwor Chandra Paudyal <ishwor.paudyal12@gmail.com>
2025-12-19 15:59:13 +0545

    Add EV sales data visualization in analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/ev_sales_2015_2025.csv")
+
+plt.plot(df["Year"], df["EV_Sales_Million"])
+plt.title("Global EV Sales Growth (2015–2025)")
+plt.xlabel("Year")
+plt.ylabel("EV Sales (Million Units)")
+plt.grid(True)
+plt.show()
 
